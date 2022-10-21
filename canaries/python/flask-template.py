859b6e90d21ea0b8bf805631229e4c161aced65b
François Proulx <76956526+fproulx-boostsecurity@users.noreply.github.com>
2022-10-21 15:24:17 -0400

    Update flask-template.py

--- canaries/python/flask-template.py
+++ canaries/python/flask-template.py
@@ -18,19 +18,19 @@ def error(e):
     return flask.render_template_string(template), 404
 
 
-# @app.route("/error4")
-# def error4(e):
-#     template4 = """
-# {  extends "layout.html"  }
-# {  block body  }
-#     <div class="center-content error">
-#         <h1>Oops! That page doesn't exist.</h1>
-#         <h3>
-# """
-#     template4 += request.url
-#     template4  +=    """
-# </h3>
-# </div>
-# {  endblock  }
-# """
-#     rendered = flask.render_template_string(template4)
+@app.route("/error4")
+def error4(e):
+    template4 = """
+{  extends "layout.html"  }
+{  block body  }
+    <div class="center-content error">
+        <h1>Oops! That page doesn't exist.</h1>
+        <h3>
+"""
+    template4 += request.url
+    template4  +=    """
+</h3>
+</div>
+{  endblock  }
+"""
+    rendered = flask.render_template_string(template4)
